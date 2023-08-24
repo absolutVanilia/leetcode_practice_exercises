78784d990c67febac34dc219c0aadb4381742ef3
absolutVanilia <133985975+absolutVanilia@users.noreply.github.com>
2023-08-24 16:41:52 -0500

    Time: 1031 ms (76.15%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,4 +1,4 @@
-select concat(year(trans_date),'-', date_format(trans_date, '%m')) as month, 
+select date_format(trans_date, '%Y-%m') as month, 
         country,
         count(*) as trans_count,
         sum(if(state = 'approved', 1, 0)) as approved_count,
