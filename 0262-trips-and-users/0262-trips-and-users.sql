7cd600bc65f8762330e5baaee6aed3d984db03df
absolutVanilia <133985975+absolutVanilia@users.noreply.github.com>
2023-08-25 18:36:44 -0500

    Time: 1135 ms (79.94%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -5,7 +5,5 @@ with temp_table (status, request_at) as
         where u.banned != 'yes' and u2.banned != 'yes' and request_at between '2013-10-01' and '2013-10-03')
 
 select request_at as Day,
-       round(avg(case when status = 'cancelled_by_driver' then 1
-                when status = 'cancelled_by_client' then 1
-                else 0 end), 2) as 'Cancellation Rate' from temp_table
+       round(avg(case when status like 'cancelled%' then 1 else 0 end), 2) as 'Cancellation Rate' from temp_table
 group by request_at
